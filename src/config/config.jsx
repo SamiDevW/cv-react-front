a2eb574743adac19b8e7cc8ab0f2293b5b4eca89
SamiDevW <SamiDevW@users.noreply.github.com>
2024-12-14 18:20:25 +0100

    fixed url

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,5 @@
-// export const url = "https://cv-react-api.onrender.com/";
-export const url = "http://localhost:3500/";
+export const url = "https://cv-react-api.onrender.com/";
+// export const url = "http://localhost:3500/";
 export const successSnackbar = {
 	variant: "success",
 	autoHideDuration: 2000,
